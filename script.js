a87250d9cb28c0b9c020dc2c87d8148cd250bb64
victorthury <victor.hauache@tino.com.br>
2025-04-01 13:51:48 -0400

    feat: turn into pwa

--- script.js
+++ script.js
@@ -72,3 +72,10 @@ function gerarTabela() {
 
   montarTabela(volumes, momentos);
 }
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./sw.js")
+    .then(() => console.log("Service Worker registrado!"))
+    .catch((err) => console.error("Erro ao registrar Service Worker:", err));
+}
---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "cafe-pwa-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+];
+
+// Instalando o Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Interceptando requisições para servir do cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
